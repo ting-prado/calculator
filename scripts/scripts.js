b673849cd118e3cb1f96b6af2e8b5fc4972ca648
Ethel <ethel.prado1998@gmail.com>
2021-08-11 20:02:48 +0800

    Added keyboard support; limited character display to 19; lets user input decimals

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -33,60 +33,67 @@ function divide(a,b) {
     return a/b;
 }
 
-
 const display = document.querySelector('#display');
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => {
     key.addEventListener('click', inputNumber);
 });
+window.addEventListener('keydown', inputNumber);
 
 function inputNumber(e) {
-    if(this.id == 'num1') {
-        display.textContent += '1';
+    if(display.textContent.length < 19 ){
+        if(this.id == 'num1' || e.key == 1) {
+            display.textContent += '1';
+        }
+        else if(this.id == 'num2' || e.key == 2) {
+            display.textContent += '2';
+        }
+        else if(this.id == 'num3' || e.key == 3) {
+            display.textContent += '3';
+        }
+        else if(this.id == 'num4' || e.key == 4) {
+            display.textContent += '4';
+        }
+        else if(this.id == 'num5' || e.key == 5) {
+            display.textContent += '5';
+        }
+        else if(this.id == 'num6' || e.key == 6) {
+            display.textContent += '6';
+        }
+        else if(this.id == 'num7' || e.key == 7) {
+            display.textContent += '7';
+        }
+        else if(this.id == 'num8' || e.key == 8) {
+            display.textContent += '8';
+        }
+        else if(this.id == 'num9' || e.key == 9) {
+            display.textContent += '9';
+        }
+        else if(this.id == 'num0' || e.key == 0) {
+            display.textContent += '0';
+        }
+        else if(this.id == 'dec' || e.key == '.') {
+            if(display.textContent.includes('.') == false && display.textContent.length>0){
+                display.textContent += '.';
+            }
+        }
     }
-    else if(this.id == 'num2') {
-        display.textContent += '2';
-    }
-    else if(this.id == 'num3') {
-        display.textContent += '3';
-    }
-    else if(this.id == 'num4') {
-        display.textContent += '4';
-    }
-    else if(this.id == 'num5') {
-        display.textContent += '5';
-    }
-    else if(this.id == 'num6') {
-        display.textContent += '6';
-    }
-    else if(this.id == 'num7') {
-        display.textContent += '7';
-    }
-    else if(this.id == 'num8') {
-        display.textContent += '8';
-    }
-    else if(this.id == 'num9') {
-        display.textContent += '9';
-    }
-    else if(this.id == 'num0') {
-        display.textContent += '0';
-    }
-    else if(this.id == 'add') {
+    if(this.id == 'add' || e.key == '+') {
         display.textContent += '+';
     }
-    else if(this.id == 'sub') {
+    else if(this.id == 'sub' || e.key == '-') {
         display.textContent += '-';
     }
-    else if(this.id == 'mul') {
+    else if(this.id == 'mul' || e.key == '*') {
         display.textContent += '*';
     }
-    else if(this.id == 'div') {
+    else if(this.id == 'div' || e.key == '%' || e.key == '/') {
         display.textContent += '/';
     }
-    else if(this.id == 'dec') {
-        display.textContent += '.';
-    }
-    else if(this.id == 'del') {
+    else if(this.id == 'del' || e.key == 'Backspace') {
         display.textContent = display.textContent.slice(0, -1);
+    }
+    else if(this.id == 'clear' || e.key == '' || e.key == 'c' || e.key == 'C') {
+        display.textContent = "";
     }
 }
