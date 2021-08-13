d91241e3eafefe5e386860e0a468fcf9b8f95b8d
Ethel <ethel.prado1998@gmail.com>
2021-08-14 07:58:46 +0800

    Operates when equal button clicked/pressed; Added keydown effects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,7 @@
 let operand1 = 0,
     operand2 = 0,
-    answer = 0;
+    answer = 0,
+    operation = "";
 
 function operate(a, op, b) {
 if(op == '+') {
@@ -40,6 +41,7 @@ keys.forEach(key => {
     key.addEventListener('click', inputNumber);
 });
 window.addEventListener('keydown', inputNumber);
+window.addEventListener('keydown', addEffect);
 
 function inputNumber(e) {
     if(botDisplay.textContent.length < 18 ){
@@ -81,7 +83,7 @@ function inputNumber(e) {
     }
     if(botDisplay.textContent.includes('+') == false && botDisplay.textContent.includes('-') == false && botDisplay.textContent.includes('*') == false && botDisplay.textContent.includes('/') == false){
         if(this.id == 'add' || e.key == '+') {
-            operand1 = botDisplay.textContent;
+            operand1 = Number(botDisplay.textContent);
             if(operand1[operand1.length-1] == '.'){
                 topDisplay.textContent = `${operand1}0 +`
             }
@@ -128,15 +130,81 @@ function inputNumber(e) {
             topDisplay.textContent = "";
         }
     }
-    else if(this.id == 'clear' || e.key == '' || e.key == 'c' || e.key == 'C') {
+    else if(this.id == 'clear' || e.key == 'c' || e.key == 'C') {
         botDisplay.textContent = "";
         topDisplay.textContent = "";
         operand1 = 0,
-        operand2 = 2,
+        operand2 = 0,
         answer = 0;
+    }
+    else if(this.id == 'equal' || e.key == 'Enter' || e.key == '=') {
+        operand2 = Number(botDisplay.textContent);
+        operation = topDisplay.textContent[topDisplay.textContent.length-1];
+        operate(operand1, operation, operand2);
+        topDisplay.textContent = `${operand1} ${operation} ${operand2} =`;
+        botDisplay.textContent = answer;
     }
 }
 
-function changeDisplays() {
-    
+function addEffect(e){
+    let key;
+    if(e.key == '1') {
+        key = document.querySelector('#num1');
+    }
+    else if(e.key == '2') {
+        key = document.querySelector('#num2');
+    }
+    else if(e.key == '3') {
+        key = document.querySelector('#num3');
+    }
+    else if(e.key == '4') {
+        key = document.querySelector('#num4');
+    }
+    else if(e.key == '5') {
+        key = document.querySelector('#num5');
+    }
+    else if(e.key == '6') {
+        key = document.querySelector('#num6');
+    }
+    else if(e.key == '7') {
+        key = document.querySelector('#num7');
+    }
+    else if(e.key == '8') {
+        key = document.querySelector('#num8');
+    }
+    else if(e.key == '9') {
+        key = document.querySelector('#num9');
+    }
+    else if(e.key == '0') {
+        key = document.querySelector('#num0');
+    }
+    else if(e.key == 'Backspace') {
+        key = document.querySelector('#del');
+    }
+    else if(e.key == '.') {
+        key = document.querySelector('#dec');
+    }
+    else if(e.key == '+') {
+        key = document.querySelector('#add');
+    }
+    else if(e.key == '-') {
+        key = document.querySelector('#sub');
+    }
+    else if(e.key == '*') {
+        key = document.querySelector('#mul');
+    }
+    else if(e.key == '/' || e.key == '%') {
+        key = document.querySelector('#div');
+    }
+    else if(e.key == 'Enter' || e.key == '=') {
+        key = document.querySelector('#equal');
+    }
+    else if(e.key == 'C' || e.key == 'c') {
+        key = document.querySelector('#clear');
+    }
+    else return;
+    key.classList.add('selected');
+        setTimeout(function() {
+            key.classList.remove('selected');
+        }, 80);
 }
